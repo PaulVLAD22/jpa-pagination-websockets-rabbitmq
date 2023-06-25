62de376d56b593c2f924df79489b77ee609bca43
vlad <vlad.munteanu@orange.com>
2023-06-25 23:06:29 +0300

    fix: web sockerts

--- src/main/java/com/example/books/config/socket/WebSocketChatConfig.java
+++ src/main/java/com/example/books/config/socket/WebSocketChatConfig.java
@@ -16,10 +16,29 @@ public class WebSocketChatConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setUserDestinationPrefix("/user");
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableStompBrokerRelay("/topic").setRelayHost("localhost").setRelayPort(61613).setClientLogin("admin")
                 .setClientPasscode("admin")
                 .setSystemLogin("admin") // Add this line
                 .setSystemPasscode("admin"); // Add this line
     }
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(new ChannelInterceptor() {
+//            @Override
+//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//                System.out.println(channel);
+//                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+//                System.out.println(accessor.getCommand());
+//                if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+//                    String user = accessor.getUser().getName(); // Get the username
+//                    String destination = accessor.getDestination(); // Get the destination
+//                    System.out.println("User '" + user + "' subscribed to '" + destination + "'");
+//                }
+//                return message;
+//            }
+//        });
+//    }
 }
--- src/main/java/com/example/books/controller/WebSocketChatController.java
+++ src/main/java/com/example/books/controller/WebSocketChatController.java
@@ -1,30 +1,38 @@
 package com.example.books.controller;
 
 import com.example.books.model.WebSocketChatMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class WebSocketChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
     @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/javainuse")
-    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
-        System.out.println(webSocketChatMessage.getSender() + " has sent a message" + webSocketChatMessage.getContent());
+    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage,
+                                            SimpMessageHeaderAccessor headerAccessor) {
+        System.out.println(headerAccessor);
+
+        messagingTemplate.convertAndSend("/topic/public", webSocketChatMessage);
+//        messagingTemplate.convertAndSend("/user/" + webSocketChatMessage.getSender() + "/queue/reply",
+//                webSocketChatMessage);
         return webSocketChatMessage;
     }
 
     @MessageMapping("/chat.newUser")
-    @SendTo("/topic/javainuse")
     public WebSocketChatMessage newUser(@Payload WebSocketChatMessage webSocketChatMessage,
                                         SimpMessageHeaderAccessor headerAccessor) {
-        System.out.println(webSocketChatMessage.getSender() + " has joined the chat");
-        System.out.println(headerAccessor);
         headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+
+        messagingTemplate.convertAndSend("/topic/public", webSocketChatMessage);
         return webSocketChatMessage;
     }
 }
